bc412cee03e10287cea029e73d1db3287adeb932
Jonas Moss <jonas.gjertsen@gmail.com>
2023-10-17 09:53:02 +0200

    :bug: Fixed typo.

--- code/lecture02/lln.py
+++ code/lecture02/lln.py
@@ -6,3 +6,9 @@ import seaborn as sns
 rng = np.random.default_rng(seed=313)
 x = rng.uniform(0, 1, 10000)
 means = np.cumsum(x) / (np.arange(len(x)) + 1)
+
+
+mpg = sns.load_dataset("mpg")
+sigma = np.cov(mpg.iloc[:, 0:3].T)
+
+kron = np.kron(sigma, sigma.T)
